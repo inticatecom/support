2544814a6706818b2fe05cb0013ea83e4c7a63e0
Lucas Stranks <78281961+TwoBrake@users.noreply.github.com>
2025-09-08 17:58:23 -0700

    add nextjs admin stack to the monorepo for future use

---
+++ admin/src/app/layout.tsx
@@ -0,0 +1,36 @@
+// Resources
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+// Types
+import type { Metadata } from "next";
+
+// Variables
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+// Metadata
+export const metadata: Metadata = {
+  title: "Agent Panel",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
