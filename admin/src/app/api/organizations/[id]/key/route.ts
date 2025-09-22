e3faeec821e48b7f33e27d9277fa73c89e1bce20
Lucas Stranks <78281961+TwoBrake@users.noreply.github.com>
2025-09-22 15:29:23 -0700

    add regen api key route

--- admin/src/app/api/organizations/[id]/key/route.ts
+++ admin/src/app/api/organizations/[id]/key/route.ts
@@ -1,6 +1,7 @@
 // Resources
 import { auth } from "@/auth";
-import { getOrg } from "@/lib/utility";
+import { getOrg, prisma } from "@/lib/utility";
+import { v7 as uuid } from "uuid";
 
 /**
  * Fetches the currently set API key.
@@ -16,4 +17,30 @@ export async function GET(
   if (!("name" in org)) return org;
 
   return new Response(org.apiKey); // Return the API key to the user if all checks pass.
+}
+
+/**
+ * Regenerate the API key.
+ */
+export async function PATCH(
+  _: Request,
+  ctx: RouteContext<"/api/organizations/[id]">
+) {
+  const session = await auth(); // Fetch the session.
+
+  // Validate the permissions and fetch the organization.
+  const org = await getOrg(session, ctx, "owner");
+  if (!("name" in org)) return org;
+
+  // Update API key.
+  const updated = await prisma.organization.update({
+    where: {
+      id: org.id,
+    },
+    data: {
+      apiKey: uuid(),
+    },
+  });
+
+  return new Response(updated.apiKey); // Return new API key.
 }
